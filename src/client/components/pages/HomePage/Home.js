fd00f9ae070dddd8c9c8b81046f1039c55b930eb
NataliaS1994 <natalia.sowaa@gmail.com>
2022-01-07 22:23:51 +0100

    add base components for the meal app

---
+++ src/client/components/pages/HomePage/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="about-page">
+      <h1 className="about-page-h1"> Unforgettable culinary experiences.</h1>
+      <p>Join a fantastic culinary experiences led by passionate chefs. </p>
+      <img
+          src="https://www.stephenconroy.com/images/GALLERYVIEW_575_MissSaigon_Hero_web.jpg"
+          alt="Meal Picture"
+          width="1150px"
+          height="500px"
+          width="100%"
+          height="49%"
+        />
+      
+    </div>
+  );
+}
+
+export default Home;
