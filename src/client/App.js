c5b418fb27c3d1ecafd5ea52d09d0ffa1feb2369
NataliaS1994 <natalia.sowaa@gmail.com>
2022-01-09 16:13:53 +0100

    add reservations page to the navigation

--- src/client/App.js
+++ src/client/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import Meals from './components/pages/MealsPage/Meals';
 import AddMeals from './components/pages/AddMeal/AddMeal';
+import Reservations from './components/pages/Reservations/Reservations';
 import Navbar from './components/Navbar';
 import Footer from './components/pages/Footer/Footer';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/meals" exact component={Meals} />
         <Route path="/add-meal" exact component={AddMeals} />
+        <Route path="/reservations" exact component={Reservations} />
       </Switch>
       <Footer />
     </Router>
